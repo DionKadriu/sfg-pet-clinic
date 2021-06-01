28429ee14c81bcf4254cb4162d73746399d2e44f
Dion Kadriu <dik00028@students.stir.ac.uk>
2021-06-01 11:49:58 +0200

    Created the superclass of the database and fixed the broken links. Closes #27, closes #26

--- pet-clinit-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexControllers.java
+++ pet-clinit-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexControllers.java
@@ -3,11 +3,18 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexControllers {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+    @RequestMapping("/oups")
+    public String oupsHnadler(){
+
+        return "notimplemented";
     }
 }
