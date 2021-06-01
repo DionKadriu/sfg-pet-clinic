e77dab3706dbeb071df8d97d0cb30f6f0d5132c3
Dion Kadriu <dik00028@students.stir.ac.uk>
2021-06-01 14:27:06 +0200

    Create JPA repositories Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VisitRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VisitRepository extends CrudRepository<Visit,Long> {
+}
